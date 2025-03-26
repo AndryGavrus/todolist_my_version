7cf34d640fc1ac62c0bf61f81daba8038fb8caf8
Andrey_Work <andry.gavrus@gmail.com>
2025-03-26 17:04:59 +0200

    made 2 les

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import './App.css'
 import { TodolistItem } from './TodolistItem'
 
@@ -7,19 +8,43 @@ export type Task = {
   isDone: boolean
 }
 
+export type FilterValues = 'all' | 'active' | 'completed'
+
 export const App = () => {
-  const tasks1: Task[] = [
+  const [tasks, setTasks] = useState([
     { id: 1, title: 'HTML&CSS', isDone: true },
     { id: 2, title: 'JS', isDone: true },
     { id: 3, title: 'ReactJS', isDone: false },
-  ]
+    { id: 4, title: 'Redux', isDone: false },
+    { id: 5, title: 'Typescript', isDone: false },
+    { id: 6, title: 'RTK query', isDone: false },
+  ])
 
-  const tasks2: Task[] = []
+  const [filter, setFilter] = useState<FilterValues>('all')
+
+  let filteredTasks = tasks
+  if (filter === 'active') {
+    filteredTasks = tasks.filter(task => !task.isDone)
+  }
+  if (filter === 'completed') {
+    filteredTasks = tasks.filter(task => task.isDone)
+  }
+
+  const changeFilter = (filter: FilterValues) => {
+    setFilter(filter)
+  }
+
+  const deleteTask = (taskId: number) => {
+    const filteredTasks = tasks.filter(t => t.id !== taskId)
+    return setTasks(filteredTasks)
+  }
 
   return (
     <div className="app">
-      <TodolistItem title="What to learn" tasks={tasks1} />
-      <TodolistItem title="Songs" tasks={tasks2} />
+      <TodolistItem title="What to learn"
+        tasks={filteredTasks}
+        deleteTask={deleteTask} 
+        changeFilter={changeFilter}/>
     </div>
   )
 }
--- src/TodolistItem.tsx
+++ src/TodolistItem.tsx
@@ -1,18 +1,25 @@
-import { Task } from "./App"
+import { FilterValues, Task } from "./App"
+import { Button } from "./Button"
 
 
 type Props = {
     title: string
     tasks: Task[]
+    deleteTask: (taskId: number) => void
+    changeFilter: (filter: FilterValues) => void
 }
 
-export const TodolistItem = ({ title, tasks }: Props) => {
+export const TodolistItem = ({ title, 
+                                tasks, 
+                                deleteTask, 
+                                changeFilter
+                            }: Props) => {
     return (
         <div>
             <h3>{title}</h3>
             <div>
                 <input />
-                <button>+</button>
+                <Button title="+" />
             </div>
             {tasks.length === 0 ? (
                 <p>Тасок нет</p>
@@ -23,15 +30,16 @@ export const TodolistItem = ({ title, tasks }: Props) => {
                             <li key={t.id}>
                                 <input type="checkbox" checked={t.isDone} />
                                 <span>{t.title}</span>
+                                <Button title={'x'} onClick={() => deleteTask(t.id)} />
                             </li>
                         )
                     })}
                 </ul>
             )}
             <div>
-                <button>All</button>
-                <button>Active</button>
-                <button>Completed</button>
+                <Button title="All" onClick={()=>changeFilter("all")}/>
+                <Button title="Active" onClick={()=>changeFilter("active")}/>
+                <Button title="Completed" onClick={()=>changeFilter("completed")}/>
             </div>
         </div>
     )
