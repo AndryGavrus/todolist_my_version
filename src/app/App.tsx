c464252c67ec35e05cf779d50bd4b163aafaaaa7
Andrey_Work <andry.gavrus@gmail.com>
2025-05-28 17:27:12 +0300

    dop

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,35 +1,6 @@
-import { useState } from 'react'
 import './App.css'
-import { TodolistItem } from '../TodolistItem'
-import { CreateItemForm } from '../CreateItemForm'
-import AppBar from '@mui/material/AppBar'
-import Toolbar from '@mui/material/Toolbar'
-import IconButton from '@mui/material/IconButton'
-import MenuIcon from '@mui/icons-material/Menu'
-import Container from '@mui/material/Container'
-import Grid from '@mui/material/Grid'
-import Paper from '@mui/material/Paper'
-import { containerSx } from '../TodolistItem.styles'
-import { NavButton } from '../NavButton'
-import { createTheme, CssBaseline, Switch, ThemeProvider } from '@mui/material'
-import {
-    changeTodolistFilterAC,
-    changeTodolistTitleAC,
-    createTodolistAC,
-    deleteTodolistAC,
-} from '../model/todolists-reducer'
-import {
-    changeTaskStatusAC,
-    changeTaskTitleAC,
-    createTaskAC,
-    deleteTaskAC,
-} from '../model/tasks-reducer'
-import { useAppDispatch } from './hooks/useAppDispatch'
+import { CssBaseline, ThemeProvider } from '@mui/material'
 import { useAppSelector } from './hooks/useAppSelector'
-import { selectTasks } from '../model/tasks-selectors'
-import { selectTodolists } from '../model/todolists-selectors'
-import { changeThemeModeAC, ThemeMode } from './app-reducer'
-import { useSelector } from 'react-redux'
 import { selectThemeMode } from './app-selectors'
 import { getTheme } from '../common/theme/theme'
 import { Header } from '@/common/components/Header/Header'
